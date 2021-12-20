a52155db803d4d0cf2fd7e54b5c6b740895a3cda
iconbit455@gmail.com <ask4r.safiullin@yandex.ru>
2021-12-20 18:36:22 +0300

    get all file from directory

--- counter.js
+++ counter.js
@@ -1,13 +1,27 @@
 const minimist = require('minimist')
 const fs = require('fs')
 const path = require('path')
+const glob = require('glob')
 
+const allowedExt = ['js', 'py', 'c', 'cpp']
 
+const searchSourceFiles = (_path) => {
+
+    allowedExt.forEach(ext => {
+        glob(`${_path}/**/!(node_modules|venv)/*.${ext}`, (er, files) => {
+            files.forEach(el => {
+                if (!el.match('venv') && !el.match('node_modules')) {
+                    console.log(path.parse(el));
+                }
+            })
+        })
+    })
+}
 
 const validateArgs = (args) => {
     if ('dir' in args) {
         if (path.isAbsolute(args.dir)) {
-            
+            searchSourceFiles(args.dir)
         }else{
             console.log('Path not absolute')
         }
